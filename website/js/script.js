b28c114a434817b1d11529ac4ab9eded3229bbe1
Merex Horwitz <merex.horwitz@scsstudent.org>
2026-02-10 09:35:47 -0800

    feat:worked on more server stuff

--- website/js/script.js
+++ website/js/script.js
@@ -1,12 +1,4 @@
-const url = `https://student-api-proxy.onrender.com/api/spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?tags=vegetarian%2Cdessert&number=1`;
-const options = {
-  method: "GET",
-  headers: {
-    "X-API-Key":
-      "b87763accfa78dbfe3d067630244d5bbfef26bd4f7f6e025331721ab94a4e826",
-  },
-};
-
+const recipesList = document.querySelector(".recipes-list");
 const searchRecipe = async (searchTerm) => {
   const url = `https://student-api-proxy.onrender.com/api/spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?tags=${searchTerm}&number=1`;
   const options = {
@@ -17,14 +9,22 @@ const searchRecipe = async (searchTerm) => {
     },
   };
 
-  // const response = await fetch(url, options);
-  // const result = await response.json();
-  const data = getSampleRecipe(); //result.recipe;
+  const response = await fetch(url, options);
+  const result = await response.json();
+  const data = result.data;
   const recipes = data.recipes;
+
+  recipes.innerHTML = "";
 
   console.log(data);
 
   recipes.forEach((recipe) => {
+    const listItem = `<li class="list-group-item">
+        <h4>RECIPE:</h4>
+        <div class="recipe-title">${recipes.title}</div>
+        <div class="recipe-ingredients">${recipes.extendedIngredients}</div>
+        <div class="recipe-instructions">${recipe.instructions} </div>`;
+    recipesList.insertAdjacentHTML("beforeend", listItem);
     // resultsDiv.innerHTML += `<p>${recipe.title}</p>`;
     console.log(recipe);
   });
